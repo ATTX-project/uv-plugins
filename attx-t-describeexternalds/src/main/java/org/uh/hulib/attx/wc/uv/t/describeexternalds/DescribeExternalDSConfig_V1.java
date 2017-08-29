717eddc7aa2bf881e8b623fdfafda2814705e1bc
jkesanie <jkesanie@lm5-hulib-1.hulib.helsinki.fi>
2017-08-29 13:51:12 +0300

    First "working" version.

---
+++ attx-t-describeexternalds/src/main/java/org/uh/hulib/attx/wc/uv/t/describeexternalds/DescribeExternalDSConfig_V1.java
@@ -0,0 +1,45 @@
+package org.uh.hulib.attx.wc.uv.t.describeexternalds;
+
+/**
+ * Configuration class for DescribeExternalDataSource.
+ *
+ * @author Unknown
+ */
+public class DescribeExternalDSConfig_V1 {
+
+    public DescribeExternalDSConfig_V1() {
+
+    }
+
+        
+    private String title = "";
+    private String description = "";
+    private String licence = "";
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLicence() {
+        return licence;
+    }
+
+    public void setLicence(String licence) {
+        this.licence = licence;
+    }
+    
+    
+    
+}
