3fe50ad580c0db6b891288b276e169c513716faf
jkesanie <jkesanie@lm5-hulib-1.hulib.helsinki.fi>
2017-10-25 08:54:50 +0300

    changed GM input to reflect the changes made to the service message structure.

--- uv-common/src/main/java/org/uh/hulib/attx/wc/uv/common/pojos/GraphManagerInput.java
+++ uv-common/src/main/java/org/uh/hulib/attx/wc/uv/common/pojos/GraphManagerInput.java
@@ -10,83 +10,61 @@ import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-    public class GraphManagerInput {
+public class GraphManagerInput {
 
-        public GraphManagerInput() {}
-        
-        @JsonProperty("contentType")
-        private String contentType;
-        @JsonProperty("inputType")
-        private String inputType;
-        @JsonProperty("namedGraph")
-        private String namedGraph;
-        @JsonProperty("input")
-        private String input;
-        @JsonProperty("activity")
-        private String activity;
-        @JsonIgnore
-        private Map<String, Object> additionalProperties = new HashMap<String, Object>();
-
-        @JsonProperty("contentType")
-        public String getContentType() {
-            return contentType;
-        }
-
-        @JsonProperty("contentType")
-        public void setContentType(String contentType) {
-            this.contentType = contentType;
-        }
-
-        @JsonProperty("inputType")
-        public String getInputType() {
-            return inputType;
-        }
-
-        @JsonProperty("inputType")
-        public void setInputType(String inputType) {
-            this.inputType = inputType;
-        }
-
-        @JsonProperty("namedGraph")
-        public String getNamedGraph() {
-            return namedGraph;
-        }
-
-        @JsonProperty("namedGraph")
-        public void setNamedGraph(String namedGraph) {
-            this.namedGraph = namedGraph;
-        }
-
-        @JsonProperty("input")
-        public String getInput() {
-            return input;
-        }
-
-        @JsonProperty("input")
-        public void setInput(String input) {
-            this.input = input;
-        }
-
-        @JsonProperty("activity")
-        public String getActivity() {
-            return activity;
-        }
-
-        @JsonProperty("activity")
-        public void setActivity(String activity) {
-            this.activity = activity;
-        }
-
-        @JsonAnyGetter
-        public Map<String, Object> getAdditionalProperties() {
-            return this.additionalProperties;
-        }
-
-        @JsonAnySetter
-        public void setAdditionalProperty(String name, Object value) {
-            this.additionalProperties.put(name, value);
-        }
-
+    public GraphManagerInput() {
     }
+
+    @JsonProperty("activity")
+    private String activity;
+    @JsonProperty("targetGraph")
+    private String targetGraph;
+    @JsonProperty("sourceData")
+    private List<Source> sourceData = null;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("activity")
+    public String getActivity() {
+        return activity;
+    }
+
+    @JsonProperty("activity")
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+
+    @JsonProperty("targetGraph")
+    public String getTargetGraph() {
+        return targetGraph;
+    }
+
+    @JsonProperty("targetGraph")
+    public void setTargetGraph(String targetGraph) {
+        this.targetGraph = targetGraph;
+    }
+
+    @JsonProperty("sourceData")
+    public List<Source> getSourceData() {
+        return sourceData;
+    }
+
+    @JsonProperty("sourceData")
+    public void setSourceData(List<Source> sourceData) {
+        this.sourceData = sourceData;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
